2dadd571a0ef656ce7f2062103da4d43366a7c03
khyunjiee <khyunjiee@gmail.com>
2020-01-10 00:31:47 +0900

    [Update] Swift 반복문 예제

--- RepetitiveStatement.playground/Contents.swift
+++ RepetitiveStatement.playground/Contents.swift
@@ -15,4 +15,33 @@ for year in 1940...2017 {
 //    print("개별 문자는 \(char)입니다.")
 //}
 
+let size = 5
+let padChar = "0"
+var keyword = "3"
 
+for _ in 1...size {
+    keyword = padChar + keyword
+}
+
+print("\(keyword)")
+
+for i in 1..<10 {
+    for j in 1..<10 {
+        print("\(i) X \(j) = \(i * j)")
+    }
+}
+
+var n = 2
+while n < 1000 {
+    n = n * 2
+}
+print("n = \(n)")
+
+n = 1024
+
+repeat {
+    n = n * 2
+}
+while n < 1000
+
+print("n = \(n)")
