c911298fdf36a014c3456237df53af6915520060
khyunjiee <khyunjiee@gmail.com>
2020-01-09 23:57:38 +0900

    [Upload] Swift 반복문 예제

---
+++ RepetitiveStatement.playground/Contents.swift
@@ -0,0 +1,18 @@
+for row in 1...5 {
+    print(row)
+}
+
+for row in 1...9 {
+    print("2 X \(row) = \(row * 2 )")
+}
+
+for year in 1940...2017 {
+    print("\(year) 년도")
+}
+
+//var lang: String = "swift"
+//for char in lang.characters {
+//    print("개별 문자는 \(char)입니다.")
+//}
+
+
