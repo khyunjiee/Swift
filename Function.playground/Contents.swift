dd22013f40126dbfa9b509cd8766a730eaaf0bc5
khyunjiee <khyunjiee@gmail.com>
2020-03-05 23:47:34 +0900

    [Upload] 함수

--- Function.playground/Contents.swift
+++ Function.playground/Contents.swift
@@ -15,3 +15,15 @@ func sayHelloWithName(name: String) -> String {
     let returnValue = "\(name)님, 안녕하세요."
     return returnValue
 }
+
+func increementBy(amount: Int, numberOfTimes: Int) {
+    var count = 0
+    count = amount * numberOfTimes
+}
+
+printHello()
+
+let inputName = "홍길동"
+printHelloWithName(name: inputName)
+
+increementBy(amount: 5, numberOfTimes: 2)
